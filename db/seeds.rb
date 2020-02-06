1ecae0d8a988bbfbedb47725c0599639a1929c2f
nicholasmontgomery <na_montgomery@yahoo.co.uk>
2020-02-06 10:32:00 +0000

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+require_relative('../models/student')
+
+# Student.delete_all()
+
+student1 = Student.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house" => "Gryffindor",
+  "age" => 16
+  })
+
+  student2 = Student.new({
+    "first_name" => "Draco",
+    "last_name" => "Malfoy",
+    "house" => "Slytherin",
+    "age" => 15
+  })
+
+    student3 = Student.new({
+      "first_name" => "Hermione",
+      "last_name" => "Granger",
+      "house" => "Gryffindor",
+      "age" => 17
+    })
+
+student1.save()
+student2.save()
+student3.save()
